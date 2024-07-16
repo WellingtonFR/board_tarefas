bd0ad35c143a3b9514690c24bda2bb4cc51b2861
WellingtonFR <wellingtondfr@gmail.com>
2024-07-15 23:09:40 -0300

    Ajuste para rota dashboard se tornar privada e foto no lugar de botão no usuário logado

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import styles from './styles.module.css';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export function Header() {
   const { data: session, status } = useSession();
@@ -37,23 +38,38 @@ export function Header() {
               <span>{' }'}</span>
             </h1>
           </Link>
-          <Link href="/dashboard" className={styles.dashboardButton}>
-            Meu painel
-          </Link>
+          {session?.user && (
+            <Link href="/dashboard" className={styles.dashboardButton}>
+              Meu painel
+            </Link>
+          )}
         </nav>
 
         {status === 'loading' ? (
           <></>
         ) : session ? (
-          <button
-            id="buttonLogout"
-            className={styles.loginButton}
-            onMouseOver={handleMouseOverLogoutButton}
-            onMouseOut={handleMouseOutLogoutButton}
-            onClick={() => signOut()}
-          >
-            {buttonLogoutText}
-          </button>
+          <div className={styles.containerLogout} onClick={() => signOut()}>
+            <Image
+              src="https://lh3.googleusercontent.com/a/ACg8ocIAtaLHV8E0b5qh-R-rB3S-KTu7j1fSLQqkFE1Kp7M15dNiHtKq=s96-c"
+              alt="Imagem de perfil do Gogle"
+              id="profilePicture"
+              className={styles.profilePicture}
+              width={50}
+              height={50}
+            />
+
+            <div className={styles.textLogout}>SAIR</div>
+
+            {/* <button
+              id="buttonLogout"
+              className={styles.loginButton}
+              onMouseOver={handleMouseOverLogoutButton}
+              onMouseOut={handleMouseOutLogoutButton}
+              onClick={() => signOut()}
+            >
+              {buttonLogoutText}
+            </button> */}
+          </div>
         ) : (
           <button className={styles.loginButton} onClick={handleLogin}>
             Login
